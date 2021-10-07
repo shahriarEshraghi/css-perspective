28534eb0a43e93739ba4b44473cbdea7c4662a2d
shahriar <shahriareshraghi22@Gmail.com>
2021-10-07 11:01:05 +0330

    app v1

---
+++ app.js
@@ -0,0 +1,33 @@
+Vue.createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    }
+  },
+  computed: {
+    rotateTransform() {
+      return { transform: 'rotateX(' + this.rotateX + 'deg)' + 'rotateY(' + this.rotateY + 'deg)' + 'rotateZ(' + this.rotateZ + 'deg)' }
+    },
+    perspectiveTransform() {
+      return { perspective: this.perspective + 'px' }
+    }
+  },
+  methods: {
+    resetAttrs() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    copyAttrs(){
+      const attr = 'transform:' +'rotateX(' + this.rotateX + 'deg)' + 'rotateY(' + this.rotateY + 'deg)' + 'rotateZ(' + this.rotateZ + 'deg);' + 'perspective: ' + this.perspective + 'px' ;
+      console.log(attr)
+    }
+
+
+  }
+
+}).mount('#app')
